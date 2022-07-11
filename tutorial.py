6f898abc61560e4e392b00c30b2b7200faa70053
Jim <jim@jam.sessionsnet.org>
2022-07-11 05:28:37 -0700

    add func that initializes screen

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,12 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
