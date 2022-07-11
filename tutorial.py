0c7ceffe78ea9c02acbdee42f143b2eae8412e9d
Jim <jim@jam.sessionsnet.org>
2022-07-11 05:31:01 -0700

    remove screen init stuff from func main

--- tutorial.py
+++ tutorial.py
@@ -11,9 +11,6 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr(1, 0, "hello world")
-    stdscr.refresh()
     stdscr.getkey()
 
 wrapper(main)
