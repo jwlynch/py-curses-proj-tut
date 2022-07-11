c6e9d4dd6100b971a92ae3b9b2e249d6996a5f9c
Jim <jim@jam.sessionsnet.org>
2022-07-11 05:32:28 -0700

    put key gotten at start into a var

--- tutorial.py
+++ tutorial.py
@@ -11,7 +11,8 @@ def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.getkey()
+
+    key = stdscr.getkey()
 
 wrapper(main)
 
