37d02bbdc12b266daa9b592af0078fcf2583ce53
Jim <jim@jam.sessionsnet.org>
2022-07-11 20:19:33 -0700

    wait for key at end of start_screen instead of in main

--- tutorial.py
+++ tutorial.py
@@ -6,13 +6,13 @@ def start_screen(stdscr):
     stdscr.addstr("Welcome to the Speed Typing Test!")
     stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
+    stdscr.getkey()
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-    key = stdscr.getkey()
 
 wrapper(main)
 
