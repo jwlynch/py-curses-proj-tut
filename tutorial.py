dbb423f6cf9d3513e9b98b15be65b937cad7a8f9
Jim <jim@jam.sessionsnet.org>
2022-07-11 21:05:15 -0700

    position cursor for chars that user types

--- tutorial.py
+++ tutorial.py
@@ -44,6 +44,9 @@ def wpm_test(stdscr):
         stdscr.clear()
         stdscr.addstr(target_text)
 
+        # position cursor to start of second line (for what user types)
+        stdscr.addstr(1, 0, "")
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
