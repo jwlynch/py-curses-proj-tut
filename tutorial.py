6c09443f869965207335cc7a1f0bede3a3e4bc1b
Jim <jim@jam.sessionsnet.org>
2022-07-11 20:27:07 -0700

    start working on func that runs the actual typing test, wpm_test()

--- tutorial.py
+++ tutorial.py
@@ -8,6 +8,10 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+def wpm_test(stdscr):
+    target_text = "hello world here is some text for this app"
+    current_text = []
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
