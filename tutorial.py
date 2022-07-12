54c1ad87fe591899ff336b52c6999b90c1e99e8a
Jim <jim@jam.sessionsnet.org>
2022-07-11 20:33:44 -0700

    start working on loop in wpm_test() that gets keys from user

--- tutorial.py
+++ tutorial.py
@@ -16,6 +16,11 @@ def wpm_test(stdscr):
     stdscr.addstr(target_text)
     stdscr.refresh()
 
+    while True:
+        key_is_backspace_p = False
+        key_is_esc_p = False
+        key = stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
