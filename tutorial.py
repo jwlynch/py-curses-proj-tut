a0756c67f343d0c846144d7a136ec3a539d87d85
Jim <jim@jam.sessionsnet.org>
2022-07-11 23:42:01 -0700

    if index from enum. is in range of target...

--- tutorial.py
+++ tutorial.py
@@ -13,6 +13,8 @@ def display_text(stdscr, target, current, wpm=0):
 
     for i, char in enumerate(current):
         stdscr.addstr(0, i, char, color_pair(1))
+        if i < len(target):
+        else:
 
 def wpm_test(stdscr):
     target_text = "hello world here is some text for this app"
