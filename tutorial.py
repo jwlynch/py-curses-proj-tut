434651ecba6fd00113d383e7cce2a0d7e5b59114
Jim <jim@jam.sessionsnet.org>
2022-07-11 20:55:11 -0700

    if key is special, take appropriate action for each

--- tutorial.py
+++ tutorial.py
@@ -31,6 +31,12 @@ def wpm_test(stdscr):
             if key in ("KEY_BACKSPACE"):
                 key_is_backspace_p = True
 
+        # interpret (possibly special) key
+        if key_is_backspace_p:
+            current_text.pop()
+        elif key_is_esc_p:
+            break
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
